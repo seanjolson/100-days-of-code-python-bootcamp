d2e09a87325eb90dde3b25f63b21f08cc777eb8a
Sean Olson <olsonsean1997@gmail.com>
2026-06-16 15:43:17 -0700

    Create band_name_generator.py

---
+++ Day 1 - Working with Variables in Python to Manage Data/band_name_generator.py
@@ -0,0 +1,11 @@
+# Welcome user to Band Name Generator
+print("Welcome to the Band Name Generator.")
+
+# Ask user for the city they grew up in
+city = input("What's the name of the city you grew up in?\n")
+
+# Ask user for the name of their pet
+pet = input("What's your pet's name?\n")
+
+# Generate band name based on the city and pet name
+print("Your band name could be " + city + " " + pet + ".")
